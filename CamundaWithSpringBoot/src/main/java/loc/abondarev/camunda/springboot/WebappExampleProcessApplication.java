a7414f38e59dcc24ba3407cb73e51bc94bd4a3e6
BonDyka <bondarew2507@gmail.com>
2020-06-13 21:45:41 +0300

    CamundaSpringBoot. Step-1
    Setup a Spring Boot application project

---
+++ CamundaWithSpringBoot/src/main/java/loc/abondarev/camunda/springboot/WebappExampleProcessApplication.java
@@ -0,0 +1,11 @@
+package loc.abondarev.camunda.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappExampleProcessApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebappExampleProcessApplication.class, args);
+    }
+}
