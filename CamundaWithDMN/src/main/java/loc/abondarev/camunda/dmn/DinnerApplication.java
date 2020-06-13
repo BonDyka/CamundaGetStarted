c2cd0e7df392da54023ffb7981f588ab4c55fe66
BonDyka <bondarew2507@gmail.com>
2020-06-13 17:37:26 +0300

    CamundaDMN. Step-2
    Add process application class and deployment descriptor

---
+++ CamundaWithDMN/src/main/java/loc/abondarev/camunda/dmn/DinnerApplication.java
@@ -0,0 +1,9 @@
+package loc.abondarev.camunda.dmn;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication
+public class DinnerApplication extends ServletProcessApplication {
+    //
+}
