7d8c27154fbe2c2e894fe74740d943c42140e596
BonDyka <bondarew2507@gmail.com>
2020-06-13 18:03:01 +0300

    CamundaDMN. Step-4
    Add PostDeploy method

--- CamundaWithDMN/src/main/java/loc/abondarev/camunda/dmn/DinnerApplication.java
+++ CamundaWithDMN/src/main/java/loc/abondarev/camunda/dmn/DinnerApplication.java
@@ -1,9 +1,28 @@
 package loc.abondarev.camunda.dmn;
 
+import org.camunda.bpm.application.PostDeploy;
 import org.camunda.bpm.application.ProcessApplication;
 import org.camunda.bpm.application.impl.ServletProcessApplication;
+import org.camunda.bpm.dmn.engine.DmnDecisionTableResult;
+import org.camunda.bpm.engine.DecisionService;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.variable.VariableMap;
+import org.camunda.bpm.engine.variable.Variables;
 
-@ProcessApplication
+@ProcessApplication(name = "Dinner App DMN")
 public class DinnerApplication extends ServletProcessApplication {
     //
+    @PostDeploy
+    public void evaluateDecisionTable(ProcessEngine processEngine) {
+        DecisionService decisionService = processEngine.getDecisionService();
+
+        VariableMap variabless = Variables.createVariables()
+                .putValue("season", "Spring")
+                .putValue("guestCount", 10);
+
+        DmnDecisionTableResult dishDecisionResult = decisionService.evaluateDecisionTableByKey("dish", variabless);
+        String desiredDish = dishDecisionResult.getSingleEntry();
+
+        System.out.println(desiredDish);
+    }
 }
