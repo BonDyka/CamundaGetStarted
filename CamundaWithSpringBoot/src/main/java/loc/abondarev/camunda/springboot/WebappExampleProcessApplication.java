475e01e485fcb45f29564499aa42a162c946ae1f
BonDyka <bondarew2507@gmail.com>
2020-06-14 08:26:20 +0300

    CamundaSpringBoot. Step-3
    Deploy and invoke BPMN process

--- CamundaWithSpringBoot/src/main/java/loc/abondarev/camunda/springboot/WebappExampleProcessApplication.java
+++ CamundaWithSpringBoot/src/main/java/loc/abondarev/camunda/springboot/WebappExampleProcessApplication.java
@@ -1,10 +1,24 @@
 package loc.abondarev.camunda.springboot;
 
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class WebappExampleProcessApplication {
+    @Autowired
+    private RuntimeService runtimeService;
+
+    @EventListener
+    private void processPostDeploy(PostDeployEvent event) {
+        runtimeService.startProcessInstanceByKey("loanApproval");
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(WebappExampleProcessApplication.class, args);
     }
