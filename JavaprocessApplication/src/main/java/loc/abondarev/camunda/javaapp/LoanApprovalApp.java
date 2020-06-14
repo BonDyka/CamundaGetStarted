0ff4a835fc35531697570ad1ad2b4f8fe7dfab41
BonDyka <bondarew2507@gmail.com>
2020-06-14 09:15:11 +0300

    Camunda Java App. Step-2
    Add process application class and deployment descriptor

---
+++ JavaprocessApplication/src/main/java/loc/abondarev/camunda/javaapp/LoanApprovalApp.java
@@ -0,0 +1,9 @@
+package loc.abondarev.camunda.javaapp;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication
+public class LoanApprovalApp extends ServletProcessApplication {
+    //empty implementation
+}
